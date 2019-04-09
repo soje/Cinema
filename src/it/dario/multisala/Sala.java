02c2aeda93271958a4e932a4e4295849aa9fecb0
Dario <thedarkvipo@gmail.com>
2019-04-09 08:14:13 +0200

    Refactor sala e spettacolo

--- src/it/dario/multisala/Sala.java
+++ src/it/dario/multisala/Sala.java
@@ -3,11 +3,80 @@ import java.util.ArrayList;
 
 public class Sala {
 
+    public Sala(ArrayList<Posto> platea, int numeroSala, boolean piena, int postiDisponibili) {
+        this.platea = platea;
+        this.numeroSala = numeroSala;
+        this.piena = piena;
+        this.postiDisponibili = postiDisponibili;
+    }
+
+    public ArrayList<Posto> getPlatea() {
+        return platea;
+    }
+
+    public int getnSala() {
+        return numeroSala;
+    }
+
+    public boolean isPiena() {
+        return piena;
+    }
+
+    public int getPostiDisponibili() {
+        return postiDisponibili;
+    }
+
+    public static int getMaxPosti() {
+        return MAX_POSTI;
+    }
+
+    public void setPlatea(ArrayList<Posto> platea) {
+        this.platea = platea;
+    }
+
+    public void setnSala(int nSala) {
+        this.numeroSala = nSala;
+    }
+
+    public void setPiena(boolean piena) {
+        this.piena = piena;
+    }
+
+    public void setPostiDisponibili(int postiDisponibili) {
+        this.postiDisponibili = postiDisponibili;
+    }
+
+    @Override
+    public String toString(){
+        String str = String.format("[platea=%s][numeroSala=%d][piena=%b][postiDisponibli=%d][MAX_POSTI=%d]", this.platea.toString(), this.numeroSala, this.piena, this.postiDisponibili, this.MAX_POSTI);
+        return getClass()+str;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if(object == null) return false;
+        if(getClass() != object.getClass()) return false;
+
+        Sala sala = (Sala) object;
+
+        return sala.platea.equals(this.platea) && (sala.numeroSala == this.numeroSala) && (sala.piena == this.piena) && (sala.postiDisponibili == this.postiDisponibili) && (sala.MAX_POSTI == this.MAX_POSTI);
+    }
+
+    @Override
+    public Sala clone(){
+        try{
+            Sala cloned = (Sala) super.clone();
+            return cloned;
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     private ArrayList<Posto> platea;
-    private int nSala;
+    private int numeroSala;
     private boolean piena;
     private int postiDisponibili;
     private final static int MAX_POSTI = 100;
-
 
 }
--- src/it/dario/multisala/Spettacolo.java
+++ src/it/dario/multisala/Spettacolo.java
@@ -1,7 +1,97 @@
 package it.dario.multisala;
 
+import java.util.GregorianCalendar;
+
 public class Spettacolo {
 
+    public Spettacolo(double prezzo, boolean politica_uno, boolean politica_due, boolean politica_tre, Sala sala, Film film, GregorianCalendar inizio) {
+        this.prezzo = prezzo;
+        this.politica_uno = politica_uno;
+        this.politica_due = politica_due;
+        this.politica_tre = politica_tre;
+        this.sala = sala;
+        this.film = film;
+        this.inizio = inizio;
+    }
 
+    public double getPrezzo() {
+        return prezzo;
+    }
 
+    public boolean isPolitica_uno() {
+        return politica_uno;
+    }
+
+    public boolean isPolitica_due() {
+        return politica_due;
+    }
+
+    public boolean isPolitica_tre() {
+        return politica_tre;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public GregorianCalendar getInizio() {
+        return inizio;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setPolitica_uno(boolean politica_uno) {
+        this.politica_uno = politica_uno;
+    }
+
+    public void setPolitica_due(boolean politica_due) {
+        this.politica_due = politica_due;
+    }
+
+    public void setPolitica_tre(boolean politica_tre) {
+        this.politica_tre = politica_tre;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public void setInizio(GregorianCalendar inizio) {
+        this.inizio = inizio;
+    }
+
+    @Override
+    public String toString(){
+        String str = String.format("[prezzo=%f][politica_uno=%b][politica_due=%b][politica_tre=%b][sala=%s][film=%s][inizio=%s]",
+                this.prezzo, this.politica_uno, this.politica_due, this.politica_tre, this.sala, this.film, this.inizio);
+        return getClass()+str;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if(object == null) return false;
+        if(getClass() != object.getClass()) return null;
+
+        Spettacolo spettacolo = (Spettacolo) object;
+
+        return ()
+    }
+
+    private double prezzo;
+    private boolean politica_uno;
+    private boolean politica_due;
+    private boolean politica_tre;
+    private Sala sala;
+    private Film film;
+    private GregorianCalendar inizio;
 }
